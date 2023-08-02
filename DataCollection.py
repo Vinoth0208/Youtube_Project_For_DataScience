3cfd40efc7f0d9aab42182005cee300c12926bf3
Vinoth <vinothchennai97@gmail.com>
2023-08-02 14:19:14 +0530

    Modified for Api key

--- DataCollection.py
+++ DataCollection.py
@@ -1,7 +1,6 @@
 from googleapiclient.discovery import build
 
 api_key = open("api.txt").read()
-
 youtube = build('youtube', 'v3', developerKey=api_key)
 def collectDataFromYoutubeForChannels(channel_id):
     channel_id=channel_id
